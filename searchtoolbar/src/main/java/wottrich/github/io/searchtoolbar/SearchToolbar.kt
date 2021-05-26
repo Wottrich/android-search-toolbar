47e481310ffaba43bae58ccbbe98ca41f9776702
Wottrich <wottrih78@gmail.com>
2021-05-25 23:51:38 -0300

    Add module searchtoolbar

---
+++ searchtoolbar/src/main/java/wottrich/github/io/searchtoolbar/SearchToolbar.kt
@@ -0,0 +1,12 @@
+package wottrich.github.io.searchtoolbar
+
+/**
+ * @author Wottrich
+ * @author wottrich78@gmail.com
+ * @since 25/05/2021
+ *
+ * Copyright © 2021 AndroidSearchToolbar. All rights reserved.
+ *
+ */
+ 
+ 
